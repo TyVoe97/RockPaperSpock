af12676ff132e1d236a26783b6a6910d61de6b56
TyVoe97 <tydud2016@gmail.com>
2018-09-20 15:39:47 -0500

    uploading first commit

---
+++ ConsoleApp1/GameFunction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class GameFunction
+
+    {
+        Console.Write("What hand position would you like to choose \n");
+        string hand = Console.ReadLine();
+
+        switch (hand)
+    }
+        case "Rock":
+            Console.WriteLine("You chose" + hand);
+            break;
+        case "Paper":
+            Console.WriteLine("You chose" + hand);
+            break;
+        case "Scissors":
+            Console.WriteLine("You chose" + hand);
+            break;
+        case "Lizard":
+            Console.WriteLine("You chose" + hand);
+            break;
+        case "Spock":
+            Console.WriteLine("You chose" + hand);
+            break;
+        }
